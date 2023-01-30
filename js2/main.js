568c7869cf369e38f7ce2310e4d4c1affdba375d
giovanni-moccaldi <giovannimoccaldi@gmail.com>
2023-01-30 15:59:33 +0100

    somma

--- js2/main.js
+++ js2/main.js
@@ -1,12 +1,13 @@
 numbers = [];
-console.log(numbers)
+// console.log(numbers)
 let ask = parseInt(prompt("scegli un numero"));
+
 
 
 
 i = 0
 arrivatia50 = false;
-while (numbers.lenght < 50 && !arrivatia50) {
+while (numbers.length > 50 && !arrivatia50) {
     const numeri = numbers[i];
     let somma = numeri + ask;
     console.log(somma)
